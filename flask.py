3b80b4733cac35763ba864bd275860edbfcbffe1
Anuj Shukla <68471872+anuj-moto@users.noreply.github.com>
2020-12-24 00:51:19 +0530

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,17 @@
+# WebApp1
+from flask import Flask ,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+@app.route("/Home")
+def Home():
+    return "<h1>Home Page</h1>"
+
+@app.route('/About')
+def About():
+    return "<h2>About Page</h2>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
